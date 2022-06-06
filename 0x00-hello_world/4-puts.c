41b7f7107cdb6c917bf93451caf6f1b80566a89a
Sivuyile <sivhans3@gmail.com>
2022-06-06 14:03:22 -0700

    Edited file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return : 0 (Always success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
