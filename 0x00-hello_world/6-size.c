53d65a3a055b72de881d809f0f55a3d06f341339
Sivuyile <sivhans3@gmail.com>
2022-06-09 10:45:14 -0700

    Added file 6, output sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the various sizes of data types on the computer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long));
+	printf(sizeof(float));
+	return (0);
+}
