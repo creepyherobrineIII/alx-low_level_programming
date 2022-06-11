aac74e779959fa17d2e0b4cc2971a4158639bccc
Sivuyile <sivhans3@gmail.com>
2022-06-11 11:28:05 -0700

    Added file 4, output alphabet except q & e

---
+++ 0x01-variables_if_else_while/4-printalphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Outputting alphabet in lowercase, except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+	int i;
+
+	i = 0;
+	letter = 'a';
+
+	while (i < 26)
+	{
+		if ((i != 4) & (i != 15))
+		{
+			putchar(letter + i);
+		}
+		else
+		{
+			continue;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
