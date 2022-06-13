b2c46ffec75f2fbf4e355bce53acff4c291d4819
Sivuyile <sivhans3@gmail.com>
2022-06-13 15:24:37 -0700

    Cleansed file 1, output alphabet using function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Output alphabet using function in seperate files
  *
  * Return: Always 0
  */
+
+void print_alphabet(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 26)
+	{
+		putchar('a' + i);
+		i++;
+	}
+	putchar('\n');
+}
 
 int main(void)
 {
