1ecfa5e6b14e0c9cf4b36df7e74ed1a31f360ba7
Sivuyile <sivhans3@gmail.com>
2022-06-13 16:04:56 -0700

    Edited header and source files

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - use print_alphabet function 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - uses file 2 to print alphabet 10 times
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
