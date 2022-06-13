8658314765006b66e435cb8fdfd7f2f4a7333926
Sivuyile <sivhans3@gmail.com>
2022-06-13 15:48:18 -0700

    Added file 2, prints alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out alphabet x10 times using header files
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int lc;
+
+	lc = 0;
+	i = 0;
+
+	while (lc < 10)
+	{
+		while (i < 26)
+		{
+			putchar('a' + i);
+			i++;
+		}
+	}
+	putchar('\n');
+}
