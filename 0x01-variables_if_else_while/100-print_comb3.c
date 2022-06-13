81ed463395867548026b32bd6c855cfa5ef9a02a
Sivuyile <sivhans3@gmail.com>
2022-06-13 11:17:34 -0700

    Added file 100, ouput 2-digit num combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Output digits where digit combinations do not repeat
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int g;
+
+	g = 0;
+	i = 0;
+
+	while (i < 10)
+	{
+		while (g < 10)
+		{
+			if (g == i)
+			{
+				g++;
+			} else
+			{
+				putchar('0' + i);
+				putchar('0' + g);
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+		}
+		g = i + 1;
+		i++;
+	}
+	putchar("\n");
+	return (0);
+}
