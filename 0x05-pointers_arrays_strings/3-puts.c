6d60206c9c66d2bdbc2c21fe1381ba3e41c97a10
Sivuyile <sivhans3@gmail.com>
2022-06-13 19:20:40 -0700

    Added file 3, imitates puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to standard out
+ *
+ * @str: parameter for string which is being outputted
+ */
+
+void _puts(char *str)
+{
+	int len;
+	int i;
+
+
+	len = _strlen(*str);
+	i = 0;
+
+	while (i <= len)
+	{
+		_putchar(*(str + i));
+	}
+
+}	
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ void _putchar(void);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
