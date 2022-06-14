6a25922fa326fa18cc2d1afc1b4fcd6d77752455
Sivuyile <sivhans3@gmail.com>
2022-06-14 11:14:16 -0700

    Added file 4, outputs string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string to standard out in reverse
+ *
+ * @s: parameter for string which is being outputted in reverse
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+	char g, n, r;
+
+	g = '\0';
+	n = '\n';
+	len = 0;
+	i = 0;
+
+	while (r != '\0')
+	{
+		r = *(s + len);
+		len++;
+	}
+
+	len = len - 1;
+
+	while (len < 0)
+	{
+		g = *(s + len);
+		_putchar(g);
+		len--;
+	}
+	_putchar(n);
+}
