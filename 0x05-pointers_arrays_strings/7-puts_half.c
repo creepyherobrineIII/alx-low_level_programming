3ad47ec03bfa000c6bd420f65036009338e3700c
Sivuyile <sivhans3@gmail.com>
2022-06-14 12:24:33 -0700

    Added file 7, outputs 2nd half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,11 @@
 void puts_half(char *str)
 {
 	int len, half_len;
-	int i;
 	char g, n, r;
 
 	g = '\0';
 	n = '\n';
 	len = 0;
-	i = 0;
 
 	while (r != '\0')
 	{
@@ -25,7 +23,7 @@ void puts_half(char *str)
 
 	len = len - 1;
 
-	if ((len % 2) = 1)
+	if ((len % 2) == 1)
 	{
 		len--;
 		half_len = (len / 2);
