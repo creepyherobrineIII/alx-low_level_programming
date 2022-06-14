c4bc346b0dfb031f02338a1f71a173361b4abe78
Sivuyile <sivhans3@gmail.com>
2022-06-14 12:22:53 -0700

    Added file 7, outputs 2nd half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * puts_half - prints string to stdout, but only half of string
+ *
+ * @str: parameter for string which is being outputted
+ */
+
+void puts_half(char *str)
+{
+	int len, half_len;
+	int i;
+	char g, n, r;
+
+	g = '\0';
+	n = '\n';
+	len = 0;
+	i = 0;
+
+	while (r != '\0')
+	{
+		r = *(str + len);
+		len++;
+	}
+
+	len = len - 1;
+
+	if ((len % 2) = 1)
+	{
+		len--;
+		half_len = (len / 2);
+	} else
+		half_len = (len / 2);
+
+	while (half_len < len)
+	{
+		g = *(str + half_len);
+		_putchar(g);
+		half_len++;
+	}
+	_putchar(n);
+}
