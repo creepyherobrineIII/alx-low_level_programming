8fd90ecc9624e51b0eaa5dfe57ceee8ba6ef48b9
Sivuyile <sivhans3@gmail.com>
2022-06-14 14:46:29 -0700

    Added file 4, checks ifchar is in alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,12 +11,14 @@
 
 int _isalpha(int c)
 {
-	if ((c < 97) | (c > 122))
+	if ((c < 65) | (c > 90))
 	{
-		if ((c < 65) | (c > 90))
+		if ((c > 90) & (c < 97) | (c > 122))
 		{
 			return (0);
+		} else
+		{
+			return (1);
 		}
-	} else
-		return (1);
+	}
 }
