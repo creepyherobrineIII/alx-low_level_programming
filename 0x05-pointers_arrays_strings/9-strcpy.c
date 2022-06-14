53310b71812977767786591d3418ec750c6ae121
Sivuyile <sivhans3@gmail.com>
2022-06-14 13:41:04 -0700

    Added file 9, copies string from one pointer to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * strcpy - copies string from one pointer to another
+ *
+ * @dest: destination pointer parameter 
+ *
+ * @src: string to be coppied pointer parameter
+ *
+ * Return: the pointer to dest
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char r;
+
+	i = 0;
+
+	while (r != '\0')
+	{
+		r = *(src + i);
+		*(dest + i) = r;
+		i++;
+	}
+	return (dest);
+}
