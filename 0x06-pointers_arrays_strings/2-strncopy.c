3a1b212740170fee9815e9d43167880170f9613f
Sivuyile <sivhans3@gmail.com>
2022-06-16 13:41:32 -0700

    Added file 2, copies string

---
+++ 0x06-pointers_arrays_strings/2-strncopy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies string to destination
+ *
+ * @dest: destination string parameter
+ * @src: string to be copied parameter
+ * @n: number of bytes to be used in copying
+ *
+ * Return: 0;
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, destlen;
+
+	i = 0;
+	destlen =  0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+		destlen++;
+	}
+
+	*(dest + destlen) = '\0';
+
+	return (dest);
+}
