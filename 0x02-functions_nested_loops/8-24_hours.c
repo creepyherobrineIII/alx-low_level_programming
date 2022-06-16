04335cb002d4976df3d273651f2d8ebbeac8f22d
Sivuyile <sivhans3@gmail.com>
2022-06-16 15:38:54 -0700

    Added file 8, outputs hours and mins in clock format

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -37,7 +37,7 @@ void jack_bauer(void)
 						_putchar(':');
 						_putchar('0' + m1);
 						_putchar('0' + m2);
-
+						_putchar('\n');
 						m2++;
 					}
 
@@ -64,6 +64,7 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar('0' + m1);
 					_putchar('0' + m2);
+					_putchar('\n');
 
 					m2++;
 				}
