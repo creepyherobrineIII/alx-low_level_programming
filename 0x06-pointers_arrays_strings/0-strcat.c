5e8f207838d3877c9a9dd1e9a137f558890b0172
Sivuyile <sivhans3@gmail.com>
2022-06-16 11:18:01 -0700

    Added file 0, testing output

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings together
+ * 
+ * @dest - destination string for short string
+ * @src - string to be concatenated
+ *
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return ('a');
+}
