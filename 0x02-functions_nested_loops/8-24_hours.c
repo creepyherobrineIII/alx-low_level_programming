867bd2ae2f3f289e2aae3e5a0d9e94cee0f4bd29
Sivuyile <sivhans3@gmail.com>
2022-06-16 15:33:41 -0700

    Added file 8, outputs hours and mins in clock format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints out the minutes in a day
+ */
+
+void jack_bauer(void)
+{
+	int h1, m1, h2, h3;
+
+	h1 = 0;
+	m1 = 0;
+	h2 = 0;
+	m2 = 0;
+
+	while (h1 <= 2)
+	{
+		h2 = 0;
+		m1 = 0;
+		m2 = 0;
+
+		if (h1 == 2)
+		{
+			while (h2 < 4)
+			{
+				m1 = 0;
+				m2 = 0;
+
+				while (m1 <= 5)
+				{
+					m2 = 0;
+
+					while (m2 <= 9)
+					{
+						_putchar('0' + h1);
+						_putchar('0' + h2);
+						_putchar(':');
+						_putchar('0' + m1);
+						_putchar('0' + m2);
+
+						m2++;
+					}
+
+					m1++;
+				}
+
+				h2++;
+			}
+		} 
+
+		while (h2 <= 9)
+		{
+			m1 = 0;
+			m2 = 0;
+
+			while (m1 <= 5)
+			{
+				m2 = 0;
+
+				while (m2 <= 9)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+
+					m2++;
+				}
+
+				m1++;
+			}
+
+			h2++;
+		}
+
+		h2++;
+
+	}
+}
