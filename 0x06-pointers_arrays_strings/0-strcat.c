6abc4e9bb706caa0abbcdc2eb75803e4b016bcab
Sivuyile <sivhans3@gmail.com>
2022-06-16 11:59:03 -0700

    Added file 0, cat strings using diff methods

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,33 @@
 
 /**
  * _strcat - concatenates two strings together
- * 
- * @dest - destination string for short string
- * @src - string to be concatenated
+ *
+ * @dest: destination string for short string
+ * @src: string to be concatenated
  *
  * Return: 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	return ('a');
+	int destlen, srclen;
+
+	srclen = 0;
+	destlen = 0;
+
+	while (*(dest + destlen) != '\0')
+	{
+		destlen++;
+	}
+
+	while (*(src + srclen) != '\0')
+	{
+		*(dest + destlen) = *(src + srclen);
+		srclen++;
+		destlen++;
+	}
+
+	*(dest + destlen) = '\0';
+
+	return (dest);
 }
