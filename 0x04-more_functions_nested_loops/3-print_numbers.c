0ed448709f2727eef49a8138f0cbaf65f4a0aedb
Sivuyile <sivhans3@gmail.com>
2022-06-16 20:39:56 -0700

    Added file 3, prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - outputs single digit numbers
+ */
+
+void print_numbers(void)
+{
+	int count;
+
+	count = 0;
+
+	while (count <= 9)
+	{
+		_putchar('0' + count);
+	}
+	_putchar('\n');
+}
