508e89f73caa84a685ea97898decde582d1554e6
Sivuyile <sivhans3@gmail.com>
2022-06-17 04:18:27 -0700

    Added file 5, print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print out numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int icount;
+	int tencount;
+
+	tencount = 0;
+	icount = 0;
+
+	while (icount <= 14)
+	{
+		if (icount >= 10)
+		{
+			tencount = 1;
+			_putchar('0' + tencount);
+			_putchar('0' + (icount - (10 * tencount)));
+		} else
+		{
+			_putchar('0' + icount);
+		}
+	}
+	_putchar('\n');
+}
