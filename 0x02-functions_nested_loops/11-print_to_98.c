54120fbaccf7b39568d1ab4c58bc306227f1daee
Sivuyile <sivhans3@gmail.com>
2022-06-16 19:03:43 -0700

    Changed position of equalization if statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,14 @@
 
 void print_to_98(int n)
 {
+	if (n == 98)
+	{
+		printf("%i", n);
+	}
+	
 	if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			if (n != 98)
 			{
@@ -26,7 +31,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			if (n != 98)
 			{
