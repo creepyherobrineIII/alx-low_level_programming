c99fee298933a58f7877f2da4589ab4abb60140a
Sivuyile <sivhans3@gmail.com>
2022-06-16 19:56:50 -0700

    Random files

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - calculate Fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int prev,curr, i, sum;
+
+	prev = 1;
+	curr = 2;
+	i = 2;
+
+	while (i < 50)
+	{
+
+}
