a702bdfe83f9090ddd26b6c5d0eeac90e76a95dc
Sivuyile <sivhans3@gmail.com>
2022-06-17 14:40:05 -0700

    Added file 7, output \ n times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - output \ to std out n times
+ *
+ * @n:  integer parameter to determine how many chars to print
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('\\');
+			i++;
+		}
+	}
+
+	_putchar('\n');
+}
