0b37eff457288c53ac97a7892f5c5147b85bc163
Sivuyile <sivhans3@gmail.com>
2022-06-17 04:26:34 -0700

    Eited file 5, added secnum var

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,20 @@ void more_numbers(void)
 {
 	int icount;
 	int tencount;
+	int secnum;
 
 	tencount = 0;
 	icount = 0;
+	secnum = 0;
 
 	while (icount <= 14)
 	{
 		if (icount >= 10)
 		{
 			tencount = 1;
+			secnum = icount - (10 * tencount);
 			_putchar('0' + tencount);
-			_putchar('0' + (icount - (10 * tencount)));
+			_putchar('0' + secnum);
 		} else
 		{
 			_putchar('0' + icount);
