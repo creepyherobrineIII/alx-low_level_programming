a9bb0c124dad2833c5d22c6a74ff79d05a61774f
Sivuyile <sivhans3@gmail.com>
2022-06-16 18:53:25 -0700

    Added file 11,tallying till 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers until reaching 98
+ *
+ * @n: integer parameter used as starting point
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n != 98)
+			{
+				printf("%i, ", n);
+			} else
+			{
+				printf("%i", n);
+			}
+			n++;
+		}
+	}
+
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+			{
+				printf("%i, ", n);
+			} else
+			{
+				printf("%i", n);
+			}
+			n--;
+		}
+	}
+
+	if (n == 98)
+	{
+		printf("%i", n);
+	}
+
+	printf("\n");
+}
