cd0f729d21b7a9019945bf9511d4e0fdc3a17701
Sivuyile <sivhans3@gmail.com>
2022-06-16 17:46:45 -0700

    Added file 5, converts lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts letter to lowercase
+ *
+ * @str: char parameter
+ *
+ * Return: str as uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+	char g;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		g = *(str + i);
+
+		if ((g >= 97) && (g <= 122))
+		{
+			g = g - 32;
+		} else
+		{
+			i++;
+		}
+
+		*(str + i) = g;
+		i++;
+	}
+
+	i--;
+
+	*(str + i) = '\0';
+}
