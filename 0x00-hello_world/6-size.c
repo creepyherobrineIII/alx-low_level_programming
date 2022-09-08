2d324a716bfdaa825f1f9dc173aa61bbbb822176
Hans <sivhans3@gmail.com>
2022-09-08 09:50:54 -0700

    Added file 6, output size of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,4 +13,6 @@ printf("Size of an int: %d bytes\n", sizeof(int));
 printf("Size of a long int: %d bytes\n", sizeof(long));
 printf("Size of a long long int: %d bytes\n", sizeof(long long));
 printf("Size of a float: %d bytes\n", sizeof(float));
+
+return (0);
 }
