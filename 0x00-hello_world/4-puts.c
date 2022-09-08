92bb02c36e6cacbe3a036a3f76d5d52caedc6cc5
Hans <sivhans3@gmail.com>
2022-09-08 09:35:39 -0700

    Added file 4, ouput string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Display string
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
