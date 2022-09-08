0eb5da51031d575a4f7488a5f969e033ee0e90a3
Hans <sivhans3@gmail.com>
2022-09-08 09:49:13 -0700

    Added file 6, output size of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Output sizes of data types for computer
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %d bytes\n", sizeof(char));
+printf("Size of an int: %d bytes\n", sizeof(int));
+printf("Size of a long int: %d bytes\n", sizeof(long));
+printf("Size of a long long int: %d bytes\n", sizeof(long long));
+printf("Size of a float: %d bytes\n", sizeof(float));
+}
