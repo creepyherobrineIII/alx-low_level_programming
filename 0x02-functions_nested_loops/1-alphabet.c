3b1b46a53755bca481b31f3d7c33ca83eac5113e
Hans <sivhans3@gmail.com>
2022-09-13 16:29:21 -0700

    Added file 1, prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ *
+ * Return: Always 0(Success)
+ */
+
+void print_alphabet(void)
+{
+int i = 0;
+
+while (i < 26)
+{
+_putchar('a' + i);
+i++;
+}
+
+_putchar('\n');
+return (0);
