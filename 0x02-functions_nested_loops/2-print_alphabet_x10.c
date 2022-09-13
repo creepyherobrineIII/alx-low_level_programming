d7586d097b5517361e6e064e8048a6e1bff04d63
Hans <sivhans3@gmail.com>
2022-09-13 16:39:05 -0700

    Fixed file errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabet in lowercase
+ * print_alphabet_x10 - Prints alphabet in lowercase
  *
  * Return: Always 0(Success)
  */
@@ -9,7 +9,7 @@
 void print_alphabet_x10(void)
 {
 int i = 0;
-int c = 1;
+int c = 0;
 
 while (c < 10)
 {
