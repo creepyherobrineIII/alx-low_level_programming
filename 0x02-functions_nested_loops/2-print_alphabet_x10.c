ce93998d94f853a0b25d460a0e4452e306f2a491
Hans <sivhans3@gmail.com>
2022-09-13 16:34:40 -0700

    Added file 2, prints alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabet in lowercase
+ *
+ * Return: Always 0(Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+int c = 1;
+
+while(c < 10)
+{
+while (i < 26)
+{
+_putchar('a' + i);
+i++;
+}
+_putchar('\n');
+c++;
+}
+}
