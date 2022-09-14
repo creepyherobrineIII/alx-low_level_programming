33beca91654bf32180a316c2e6cb3016412a29dc
Hans <sivhans3@gmail.com>
2022-09-13 19:11:38 -0700

    Added file 9, outputs 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Prints out 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+int m1, m2, res;
+
+while (m1 <= 9)
+{
+	m2 = 0;
+	while (m2 <= 9)
+	{
+		res = m1 * m2;
+		if (res > 10)
+		{
+			_putchar('0' + m1);
+			_putchar('0' + (res % 10));
+		}
+
+		if (m2 == 9)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		m2++;
+	}
+	m1++;
+}
+
