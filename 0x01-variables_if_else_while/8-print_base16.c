bebcda94fa38cdb210e71bba25b649b366b3c374
Hans <sivhans3@gmail.com>
2022-09-17 11:29:39 -0700

    Added file 8, prints numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints numbers of base 16
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+	n = 0;
+
+	while (n < 6)
+	{
+		putchar('a'+ n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
