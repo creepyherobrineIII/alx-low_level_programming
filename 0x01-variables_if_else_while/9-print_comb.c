e337fc56671d78dfe18c7be0acb3972a082206fb
Hans <sivhans3@gmail.com>
2022-09-17 11:34:26 -0700

    Added file 9, prints numbers in a format

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints single digit numbers in a combination (formatted)
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 9)
+	{
+		putchar('0' + n);
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('9');
+
+	return (0);
+}
