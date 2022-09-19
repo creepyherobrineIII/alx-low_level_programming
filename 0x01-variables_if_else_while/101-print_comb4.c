04c7251427725331e2622e314850a2d21da6f6f0
Hans <sivhans3@gmail.com>
2022-09-19 10:08:20 -0700

    Added file 101, prints smallest combo of 3-digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print smallest combination of three-digit numbers
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int h = 0;
+	int t = 0;
+	int d = 0;
+
+	while (h < 10)
+	{
+		d = 0;
+		t = 0;
+		while (t < 10)
+		{
+			d = 0;
+			while (d < 10)
+			{
+				if ((t <= h) && (t < 9))
+				{
+					t++;
+				}
+				if (((h != 9)  && (t > h)) && (d > t))
+				{
+					putchar('0' + h);
+					putchar('0' + t);
+					putchar('0' + d);
+					if (h + t + d != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d++;
+			}
+			t++;
+		}
+		h++;
+	}
+	return (0);
+}
+
+
