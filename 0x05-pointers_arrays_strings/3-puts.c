98c8d03a84a71276d043dbe8647ab86bd79638dc
Hans <sivhans3@gmail.com>
2022-09-20 01:43:36 -0700

    Added file 3, prints string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Returns the string length of parameter value
+ * @str: char pointer
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		_putchar(s[c]);
+		c++;
+	}
+	_putchar('\n');
+}
