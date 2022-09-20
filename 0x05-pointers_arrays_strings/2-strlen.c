fa1eb1f6afeff0ab93fd70b7a5d56ebf3515d0bd
Hans <sivhans3@gmail.com>
2022-09-20 01:32:21 -0700

    Added file 2, returns string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the string length of parameter value
+ * @s: char pointer
+ * Return: String Length (int)
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	return (c);
+}
