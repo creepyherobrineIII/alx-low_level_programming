57c4e9f0d4b2a446c386e090373f642e4ace8e8c
Hans <sivhans3@gmail.com>
2022-09-22 13:52:11 -0700

    Added file 6, prints char n times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 0 to 14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int t, d = 0;
+
+	while (t < 10)
+	{
+		while (d < 15)
+		{
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times a char
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int l = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (l < n)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
