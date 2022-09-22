76a8d736a081a402b27551598e689f0b7fceffaf
Hans <sivhans3@gmail.com>
2022-09-22 10:42:00 -0700

    Added file 3, compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares first char of string
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: 0 (Equal strings), -int(s1 < s2), +int(s1 > s2)
+ */
+
+int _strcmp(char *s1, char *s1)
+{
+	int ret;
+
+	if (s1[0] == s2[0])
+	{
+		ret = 0;
+	}
+
+	if (s1[0] > s2[0])
+	{
+		ret = s1[0] - s2[0]
+	}
+
+	if (s1[0] < s2[0])
+	{
+		ret = s1[0] - s2[0];
+	}
+
+	return (ret);
+}
